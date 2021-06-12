0a467221e1b4a076364c0535b18e662ff4c374db
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-06-11 20:07:05 -0500

    3 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-import './style.css';
+import $ from 'jquery';
 
 window.listaCompras = [];
 
-$(document).ready(function() {
-  $('#button').click(function() {
-    var toAdd = $('input[name=itemCompras]').val();
-    $('ol').append('<li>' + toAdd + '</li>');
-    $('input').val('');
-    window.listaCompras.push(toAdd);
-    localStorage.setItem('listaCompras', JSON.stringify(window.listaCompras));
-  });
+$('#button').click(function() {
+  var toAdd = $('input[name=itemCompras]').val();
+  $('ol').append('<li>' + toAdd + '</li>');
+  $('input').val('');
+  window.listaCompras.push(toAdd);
+  localStorage.setItem('listaCompras', JSON.stringify(window.listaCompras));
+});
 
-  if (localStorage.getItem('listaCompras')) {
-    window.listaCompras = JSON.parse(localStorage.getItem('listaCompras'));
-    window.listaCompras.forEach(compra => {
-      $('ol').append('<li>' + compra + '</li>');
-    });
+if (localStorage.getItem('listaCompras')) {
+  window.listaCompras = JSON.parse(localStorage.getItem('listaCompras'));
+  window.listaCompras.forEach(compra => {
+    $('ol').append('<li>' + compra + '</li>');
+  });
+}
+
+$('input[name=itemCompras]').keyup(function(event) {
+  if (event.keyCode == 13) {
+    $('#button').click();
   }
+});
 
-  $('input[name=itemCompras]').keyup(function(event) {
-    if (event.keyCode == 13) {
-      $('#button').click();
-    }
-  });
+$('form').on('submit', function(e) {
+  e.preventDefault();
+});
 
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-  });
-
-  $(document).on('dblclick', 'li', function() {
-    $(this)
-      .toggleClass('strike')
-      .fadeOut(500, function() {
-        $(this).remove();
-      });
-  });
-
-  
+$(document).on('dblclick', 'li', function() {
+  $(this)
+    .toggleClass('strike')
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
 });
